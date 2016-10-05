dc3de73f6a1aac035fcf427f93c2b9ad1e134a41
Chris Zuber <shgysk8zer0@gmail.com>
2016-10-04 22:22:44 -0700

    Enhancements to Week class
    Now designed to scan directories using `Scan` class and create `Issue`
    from file using magic methods.

--- week.php
+++ week.php
@@ -7,20 +7,46 @@ final class Week extends \DateTime
 
 	private $_root;
 
+	public $path;
+
 	public function __construct($root, $date = null)
 	{
 		if (! is_dir($root)) {
-			throw new \InvalidArgumentExcaption("{$root} is not a directory.", 500);
+			throw new \InvalidArgumentException("{$root} is not a directory.", 500);
 		}
 
 		$this->_root  = rtrim($root, DIRECTORY_SEPARATOR);
 		parent::__construct($date);
+		if ($this->getTimestamp() > time()) {
+			throw new \InvalidArgumentException('Request made for future date', 404);
+		}
+		$this->path = $this->_root . DIRECTORY_SEPARATOR . $this;
+	}
+
+	public function __isset($section)
+	{
+		return file_exists($this->path . DIRECTORY_SEPARATOR . $section . '.pdf');
+	}
+
+	public function __get($section)
+	{
+		if ($this->__isset($section))
+		{
+			return new Issue($this, $section);
+		} else {
+			throw new \Exception("$section not found in $week.", 500);
+		}
+	}
+
+	public function scan($ext = '.pdf')
+	{
+		return new Scanner($this, $this->_root, $ext);
 	}
 
 	public function __toString()
 	{
 		$format = str_replace(' ', DIRECTORY_SEPARATOR, self::DIR);
-		return $this->_root . DIRECTORY_SEPARATOR . $this->format($format);
+		return $this->format($format);
 	}
 }
 
