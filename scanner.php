15c04e2152fca100228a42dfba419f550124a719
Chris Zuber <shgysk8zer0@gmail.com>
2016-10-04 22:23:31 -0700

    Various enhancements
    Fixes and features to work with other classes.

--- scanner.php
+++ scanner.php
@@ -3,15 +3,37 @@ namespace KVSun\WP_E_Edition;
 
 final class Scanner extends \ArrayObject
 {
-	public function __construct(Week $week, $ext = '.pdf')
+	private $_week;
+	public function __construct(Week $week, $root, $ext = '.pdf')
 	{
-		$files = glob($week . DIRECTORY_SEPARATOR . "*{$ext}");
+		$this->_week = $week;
+		$path = $root . DIRECTORY_SEPARATOR . $week;
+		if (! is_dir($path)) {
+			throw new \InvalidArgumentException("Invalid directory: '{$path}'", 500);
+		}
+		$files = glob($path . DIRECTORY_SEPARATOR . "*{$ext}");
+		$files = array_map([$this, '_trimmedPath'], $files);
 		parent::__construct($files);
 	}
 
 	public function __toString()
 	{
-		return json_encode($this->getArrayCopy());
+		return json_encode(["{$this->_week}" => $this->getArrayCopy()]);
+	}
+
+	private function _trimmedPath(&$file, $ext = '.pdf')
+	{
+		return basename($file, $ext);
+	}
+
+	private function _getIssues()
+	{
+		return array_map([$this, '_getIssue'], $this->getArrayCopy());
+	}
+
+	private function _getIssue($section)
+	{
+		return new Issue($this->_week, $section);
 	}
 }
 
